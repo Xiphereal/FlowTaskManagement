8d557996b9060cc0b021ad422e26b4349cfb9942
Xiphereal <lozanojalex@gmail.com>
2025-01-06 19:39:02 +0100

    feat: create ASP.NET backend

---
+++ Backend/Backend/Controllers/ProjectController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using Task = Backend.Domain.Task;
+
+namespace Backend.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ProjectController : ControllerBase
+{
+    private readonly ILogger<ProjectController> _logger;
+
+    public ProjectController(ILogger<ProjectController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetTasks")]
+    public IEnumerable<Task> Get()
+    {
+        return
+        [
+            new Task("A task", "Dummy description"),
+            new Task("Another task", "Dummy description"),
+            new Task("Yet another task", "Dummy description"),
+        ];
+    }
+}
---
+++ Backend/Backend/Domain/Task.cs
@@ -0,0 +1,13 @@
+namespace Backend.Domain;
+
+public class Task
+{
+    public Task(string name, string description)
+    {
+        Name = name;
+        Description = description;
+    }
+
+    public string Name { get; private set; }
+    public string Description { get; private set; }
+}
