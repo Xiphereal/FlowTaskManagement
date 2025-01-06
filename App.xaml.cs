a82bc0332203c960f790db58b8f51588c5b26a9d
Xiphereal <lozanojalex@gmail.com>
2025-01-06 15:48:42 +0100

    feat: show message when unhandled exception occurs

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace Desktop;
 
@@ -9,4 +10,14 @@ namespace Desktop;
 /// </summary>
 public partial class App : Application
 {
+    private void App_OnDispatcherUnhandledException(
+        object sender,
+        DispatcherUnhandledExceptionEventArgs e)
+    {
+        MessageBox.Show(
+            "An unknown internal failure has occurred. The app will be closed.",
+            "Internal failure",
+            MessageBoxButton.OK,
+            MessageBoxImage.Error);
+    }
 }
