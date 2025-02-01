8af75a5ef1dabaabf94c5b606df9f00e51921740
Xiphereal <lozanojalex@gmail.com>
2025-02-01 20:40:36 +0100

    feat: FileSystemTaskRepository_HasNoTasksByDefault

---
+++ Desktop.Tests/FileSystemTaskRepositoryTest.cs
@@ -0,0 +1,18 @@
+﻿using Desktop;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Desktop.Tests;
+
+[TestFixture]
+[TestOf(typeof(FileSystemTaskRepository))]
+public class FileSystemTaskRepositoryTest
+{
+    [Test]
+    public void HasNoTasksByDefault()
+    {
+        var sut = new FileSystemTaskRepository();
+
+        sut.All().Should().BeEmpty();
+    }
+}
---
+++ Desktop/FileSystemTaskRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+using Task = Desktop.Domain.Task;
+
+namespace Desktop;
+
+public class FileSystemTaskRepository : ITaskRepository
+{
+    public IReadOnlyList<Task> All()
+    {
+        return [];
+    }
+}
