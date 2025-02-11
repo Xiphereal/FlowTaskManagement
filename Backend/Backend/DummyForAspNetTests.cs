e08b622178aa36528195a4f23fdaaaa14440fd7d
Xiphereal <lozanojalex@gmail.com>
2025-02-11 21:19:27 +0100

    test: can call the backend from a test

---
+++ Backend/Backend/DummyForAspNetTests.cs
@@ -0,0 +1,9 @@
+﻿namespace Backend;
+
+/// <summary>
+///     This solely exists for the purposes of tests to reference any class in the project
+///     for the WebApplicationFactory.
+/// </summary>
+public class DummyForAspNetTests
+{
+}
--- Desktop.Tests/Integration/BackendTaskRepositoryTests.cs
+++ Desktop.Tests/Integration/BackendTaskRepositoryTests.cs
@@ -1,4 +1,8 @@
-﻿using Desktop.Tasks;
+﻿using System.Threading.Tasks;
+using Backend;
+using Desktop.Tasks;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
 using NUnit.Framework;
 
 namespace Desktop.Tests.Integration;
@@ -11,5 +15,21 @@ public class BackendTaskRepositoryTests
     public void CanBeInstantiated()
     {
         var sut = new BackendTaskRepository(FakeUrl);
+    }
+
+    [Test]
+    public async Task LearningTestAbout_ASP_NET_Tests()
+    {
+        var factory = new WebApplicationFactory<DummyForAspNetTests>();
+        var client = factory.CreateClient();
+        
+        var result = await client.GetAsync("/Project/");
+
+        result.IsSuccessStatusCode.Should().BeTrue();
+        var content = await result.Content.ReadAsStringAsync();
+        content.Should()
+            .Contain("A task").And
+            .Contain("Another task").And
+            .Contain("Yet another task");
     }
 }
