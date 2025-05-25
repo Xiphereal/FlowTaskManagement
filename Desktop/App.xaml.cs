71d58b712609dbaec5095b5e047adc9a0a48f6ee
Xiphereal <lozanojalex@gmail.com>
2025-05-25 14:21:47 +0200

    feat: use the Backend Task repository on the Desktop

--- Desktop/App.xaml.cs
+++ Desktop/App.xaml.cs
@@ -1,31 +1,27 @@
 ﻿using System.Windows;
 using System.Windows.Threading;
-using Desktop.Tasks;
+using Desktop.Tasks.Extensions;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Desktop;
 
 /// <summary>
-/// Interaction logic for App.xaml
+///    Interaction logic for App.xaml
 /// </summary>
 public partial class App : Application
 {
-    public static IConfigurationRoot Config { get; private set; }
-
-    public App()
-    {
-        Config = new ConfigurationBuilder()
+    private IConfigurationRoot Config { get; } =
+        new ConfigurationBuilder()
             .AddJsonFile("appsettings.json")
             .Build();
-    }
 
     protected override void OnStartup(StartupEventArgs e)
     {
         var services = new ServiceCollection();
 
         services.AddSingleton<MainWindow>();
-        services.AddSingleton<ITaskRepository, FileSystemTaskRepository>();
+        services.AddBackendTaskRepository(Config);
 
         var serviceProvider = services.BuildServiceProvider();
         serviceProvider.GetRequiredService<MainWindow>().Show();
---
+++ Desktop/Tasks/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Desktop.Tasks.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    private const string BackendUriSectionName = "BackendUri";
+
+    public static void AddBackendTaskRepository(
+        this ServiceCollection services,
+        IConfigurationRoot config)
+    {
+        if (config.GetSection(BackendUriSectionName).Value is null)
+            throw new ArgumentException(
+                "A 'BackendUri' is expected to be defined in order to communicate with the Backend");
+
+        services.AddSingleton<HttpClient>(_ =>
+        {
+            var httpClient = new HttpClient();
+            httpClient.BaseAddress =
+                new Uri(config.GetSection(BackendUriSectionName).Value!);
+
+            return httpClient;
+        });
+        services.AddSingleton<ITaskRepository, BackendTaskRepository>();
+    }
+}
