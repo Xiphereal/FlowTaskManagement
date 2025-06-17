fbe2e8b5674fda7390c90b51e86711c9b8849c86
Xiphereal <lozanojalex@gmail.com>
2025-06-17 19:26:59 +0200

    refactor: move Tasks list to view model + non-view code from code behind

--- Desktop/Project/TaskListViewModel.cs
+++ Desktop/Project/TaskListViewModel.cs
@@ -1,10 +1,38 @@
-﻿using Desktop.Common;
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.Input;
+using Desktop.Common;
+using Desktop.Tasks;
+using Task = Desktop.Domain.Task;
+using SystemTask = System.Threading.Tasks.Task;
 
 namespace Desktop.Project;
 
 public class TaskListViewModel : ViewModelBase
 {
-    public TaskListViewModel()
+    private readonly ITaskRepository taskRepository;
+    public ObservableCollection<Task> Tasks { get; } = [];
+
+    public TaskListViewModel(ITaskRepository taskRepository)
     {
+        this.taskRepository = taskRepository;
+
+        Add = new RelayCommand<TaskCreationViewModel>(taskCreationViewModel =>
+        {
+            if (taskCreationViewModel!.CreatedTask is null)
+                return;
+
+            Tasks.Add(taskCreationViewModel.CreatedTask);
+        });
+    }
+
+    public ICommand Add { get; }
+
+    public void PopulateTasks()
+    {
+        // TODO: load this async and deferred from the ctor.
+        var retrievedTasks = SystemTask.Run(taskRepository.All).Result;
+        foreach (var task in retrievedTasks)
+            Tasks.Add(task);
     }
 }
--- Desktop/Project/TasksList.xaml.cs
+++ Desktop/Project/TasksList.xaml.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.ObjectModel;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using Desktop.Tasks;
@@ -7,14 +6,11 @@ using Task = Desktop.Domain.Task;
 
 namespace Desktop.Project;
 
-using SystemTask = System.Threading.Tasks.Task;
-
 public partial class TasksList : UserControl
 {
     private readonly ITaskRepository taskRepository;
     private readonly TaskListViewModel viewModel;
     private readonly TaskCreationViewModel taskCreationViewModel;
-    private readonly ObservableCollection<Task> tasks = [];
 
     public TasksList(
         ITaskRepository taskRepository,
@@ -26,23 +22,16 @@ public partial class TasksList : UserControl
         this.taskCreationViewModel = taskCreationViewModel;
 
         InitializeComponent();
-        Tasks.ItemsSource = tasks;
+        Tasks.ItemsSource = viewModel.Tasks;
 
-        // TODO: load this async and deferred from the ctor.
-        var retrievedTasks = SystemTask.Run(taskRepository.All).Result;
-        foreach (var task in retrievedTasks)
-            tasks.Add(task);
+        viewModel.PopulateTasks();
     }
 
     private void Add(object sender, RoutedEventArgs e)
     {
-        var taskCreationWindow = new TaskCreation(taskCreationViewModel);
-        taskCreationWindow.ShowDialog();
+        new TaskCreation(taskCreationViewModel).ShowDialog();
 
-        if (taskCreationWindow.CreatedTask is null)
-            return;
-
-        tasks.Add(taskCreationWindow.CreatedTask);
+        viewModel.Add.Execute(taskCreationViewModel);
     }
 
     private void Edit(object sender, MouseButtonEventArgs e)
@@ -61,7 +50,7 @@ public partial class TasksList : UserControl
 
         var toRemove = button.DataContext as Task;
 
-        tasks.Remove(toRemove!);
+        viewModel.Tasks.Remove(toRemove!);
         taskRepository.Delete(toRemove!);
     }
 }
