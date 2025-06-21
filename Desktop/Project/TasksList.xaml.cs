68f004ca6718a0109666b810dd358143a67f1676
Xiphereal <lozanojalex@gmail.com>
2025-06-21 21:13:27 +0200

    docs: why the edit event handler is not a Command in the ViewModel

--- Desktop/Project/TasksList.xaml.cs
+++ Desktop/Project/TasksList.xaml.cs
@@ -19,6 +19,15 @@ public partial class TasksList : UserControl
             new TaskCreation(creationViewModel);
     }
 
+    /// <remarks>
+    ///     This event handler in the code behind complies with the MVVM architectural
+    ///     style: the View (which this code behind is part of) only has concerns of
+    ///     the View.
+    ///     <para>
+    ///         If this event handler had code related with the control of the use
+    ///         case, it will be moved to a ViewModel (since it is its responsibility).
+    ///     </para>
+    /// </remarks>
     private void Edit(object sender, MouseButtonEventArgs e)
     {
         if (sender is not ListBoxItem { DataContext: Task task })
