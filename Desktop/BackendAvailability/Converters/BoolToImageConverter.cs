f28eb89ca603374382dc5a400b6c98f56ab756c8
Xiphereal <lozanojalex@gmail.com>
2025-06-26 00:25:26 +0200

    feat: backend availability is shown by an image rather than text

---
+++ Desktop/BackendAvailability/Converters/BoolToImageConverter.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Desktop.BackendAvailability.Converters;
+
+public class BoolToImageConverter : IValueConverter
+{
+    public ImageSource BackendAvailable { get; set; }
+    public ImageSource BackendUnavailable { get; set; }
+
+    public object? Convert(
+        object? value,
+        Type targetType,
+        object? parameter,
+        CultureInfo culture)
+    {
+        return (bool)value ? BackendAvailable : BackendUnavailable;
+    }
+
+    public object? ConvertBack(
+        object? value,
+        Type targetType,
+        object? parameter,
+        CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+}
--- Desktop/MainWindowViewModel.cs
+++ Desktop/MainWindowViewModel.cs
@@ -6,38 +6,30 @@ namespace Desktop;
 
 public class MainWindowViewModel : ViewModelBase
 {
-    private readonly IRemoteTaskRepository backend;
-
     private static readonly TimeSpan CheckBackendAvailabilityInterval =
         TimeSpan.FromSeconds(2);
 
-    private string backendAvailability = "Available";
+    private bool isBackendAvailable;
     private readonly DispatcherTimer periodicBackendAvailabilityCheckTimer;
 
-    public string BackendAvailability
+    public bool IsBackendAvailable
     {
-        get => backendAvailability;
+        get => isBackendAvailable;
         set
         {
-            backendAvailability = value;
+            isBackendAvailable = value;
             OnPropertyChanged();
         }
     }
 
     public MainWindowViewModel(IRemoteTaskRepository backend)
     {
-        this.backend = backend;
         periodicBackendAvailabilityCheckTimer = new DispatcherTimer
         {
             Interval = CheckBackendAvailabilityInterval
         };
         periodicBackendAvailabilityCheckTimer.Tick += async (_, _) =>
-            await CheckBackendAvailability();
+            IsBackendAvailable = await backend.IsAvailable();
         periodicBackendAvailabilityCheckTimer.Start();
-    }
-
-    private async Task CheckBackendAvailability()
-    {
-        BackendAvailability = await backend.IsAvailable() ? "Available" : "Non available";
     }
 }
