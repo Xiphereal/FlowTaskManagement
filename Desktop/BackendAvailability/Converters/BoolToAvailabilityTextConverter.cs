f622c48cb71fe2b11a63d220cd557e4ff6d63335
Xiphereal <lozanojalex@gmail.com>
2025-06-26 00:33:46 +0200

    feat: tooltip for backend availability

---
+++ Desktop/BackendAvailability/Converters/BoolToAvailabilityTextConverter.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace Desktop.BackendAvailability.Converters;
+
+public class BoolToAvailabilityTextConverter : IValueConverter
+{
+    public object? Convert(
+        object? value,
+        Type targetType,
+        object? parameter,
+        CultureInfo culture)
+    {
+        return (bool)value
+            ? "The cloud storage is available"
+            : "The cloud storage is currently unavailable";
+    }
+
+    public object? ConvertBack(
+        object? value,
+        Type targetType,
+        object? parameter,
+        CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+}
