9114f04da675a3aa818b1d15cd591f15ef8189ff
Xiphereal <lozanojalex@gmail.com>
2025-07-20 02:04:22 +0200

    refactor: replace foreach with Task.WhenAll

--- Desktop/Tasks/TaskCreationViewModel.cs
+++ Desktop/Tasks/TaskCreationViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using CommunityToolkit.Mvvm.Input;
 using Desktop.Common;
 using Task = Desktop.Domain.Task;
+using SystemTask = System.Threading.Tasks.Task;
 
 namespace Desktop.Tasks;
 
@@ -17,21 +18,17 @@ public class TaskCreationViewModel : ViewModelBase
 
                 CreatedTask = new Task(taskName, taskDescription);
 
-                var anySucceeded = false;
-                foreach (var repository in taskRepositories)
-                {
-                    var creationResult = await repository.Save(CreatedTask);
+                var savingTasks = taskRepositories
+                    .Select(x => x.Save(CreatedTask));
+                var results = await SystemTask.WhenAll(savingTasks);
 
-                    if (creationResult.Succeeded)
-                        anySucceeded = true;
-                    else
-                        messageNotifier.Notify(
-                            "Task creation has failed due to " +
-                            "an internal error. The Task won't be created. " +
-                            "Please, try again later.");
-                }
+                if (results.Any(x => !x.Succeeded))
+                    messageNotifier.Notify(
+                        "Task creation has failed due to " +
+                        "an internal error. The Task won't be created. " +
+                        "Please, try again later.");
 
-                if (!anySucceeded)
+                if (results.All(x => !x.Succeeded))
                     CreatedTask = null;
 
                 closeable!.Close();
